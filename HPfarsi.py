d1ffc7aa4defd4f1e836b3aa3e5e99efb8ebfc7f
Hossein P.™ <hoss9191p@gmail.com>
2026-07-22 21:07:57 +0330

    Create HPfarsi.py

---
+++ HPfarsi.py
@@ -0,0 +1,21 @@
+chap = print
+jam = sum
+tavan = pow
+komak = help
+baz = open
+Dorost = True
+Nadorost = False
+Hich = None
+ejra = exec
+
+__all__ = [
+    "chap",
+    "jam",
+    "tavan",
+    "komak",
+    "baz",
+    "Dorost",
+    "Nadorost",
+    "Hich",
+    "ejra",
+    ]
